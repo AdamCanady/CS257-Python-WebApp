273ecc0ac25cfa7965777f96218aae78a71410b6
Adam Canady <adam@adamcanady.com>
2013-10-06 15:58:22 -0500

    Add datasource.py

---
+++ datasource.py
@@ -0,0 +1,52 @@
+# CS 257 Python Web App, Fall 2013, Jeff Ondich
+# Jiatao Cheng, Erin Wilson, and Adam Canady
+
+class DataSource:
+    def __init__(self, ...):
+        ...
+
+    def get_rooms_by_occupancy(self, occupancy):
+        '''Takes an integer and returns a list of rooms matching that occupancy'''
+
+        rooms = []
+
+        # Implementation soon...
+
+        return rooms
+
+    def get_list_of_available_rooms(self):
+        '''Returns a list of all of the rooms ResLife offers'''
+
+        rooms = []
+
+        # Implementation soon...
+
+        return rooms
+
+    def get_rooms_by_environment(self, type):
+        '''Takes an environment ('quiet','single_gender','subfree','interest_house') and
+            returns a list of matching rooms'''
+
+        rooms = []
+
+        # Implementation soon...
+
+        return rooms
+
+    def get_rooms_near_location(self, location):
+        '''Takes a location string and finds rooms close to that location'''
+
+        rooms = []
+
+        # Implementation soon...
+
+        return rooms
+
+    def get_rooms_by_location(self, location):
+        '''Takes a location string and finds rooms in that hall/house'''
+
+        rooms = []
+
+        # Implementation soon...
+
+        return rooms
