c80a241e5c2effa1b31d64a9fc2e7a7f8ec644f2
Adam Canady <adam@adamcanady.com>
2013-10-18 15:13:23 -0500

    Add SQL files and downloader

---
+++ roomdraw_process/buildings.sql
@@ -0,0 +1,62 @@
+DROP TABLE IF EXISTS buildings;
+CREATE TABLE buildings (
+  id int,
+  building text,
+  geo_lat float,
+  geo_long float
+);
+
+INSERT INTO buildings (id, building, geo_lat, geo_long) VALUES (1, Allen House, 44.460132, -93.158479);
+INSERT INTO buildings (id, building, geo_lat, geo_long) VALUES (2, Brooks House, 44.459634, -93.157958);
+INSERT INTO buildings (id, building, geo_lat, geo_long) VALUES (3, Burton Hall, 44.460522, -93.156813);
+INSERT INTO buildings (id, building, geo_lat, geo_long) VALUES (4, Cassat Hall, 44.460034, -93.151025);
+INSERT INTO buildings (id, building, geo_lat, geo_long) VALUES (5, Clader House, 44.458679, -93.157736);
+INSERT INTO buildings (id, building, geo_lat, geo_long) VALUES (6, Collier House, 44.459301, -93.158175);
+INSERT INTO buildings (id, building, geo_lat, geo_long) VALUES (7, Colwell House, 44.458795, -93.158521);
+INSERT INTO buildings (id, building, geo_lat, geo_long) VALUES (8, Davis Hall, 44.460105, -93.156682);
+INSERT INTO buildings (id, building, geo_lat, geo_long) VALUES (9, Dixon House, 44.459146, -93.158216);
+INSERT INTO buildings (id, building, geo_lat, geo_long) VALUES (10, Dow House, 44.458998, -93.158285);
+INSERT INTO buildings (id, building, geo_lat, geo_long) VALUES (11, Eugster House, 44.460032, -93.157591);
+INSERT INTO buildings (id, building, geo_lat, geo_long) VALUES (12, Evans Hall, 44.460518, -93.149714);
+INSERT INTO buildings (id, building, geo_lat, geo_long) VALUES (13, Faculty Club, 44.458964, -93.149338);
+INSERT INTO buildings (id, building, geo_lat, geo_long) VALUES (14, Faculty Club Annex, 44.459025, -93.149917);
+INSERT INTO buildings (id, building, geo_lat, geo_long) VALUES (15, Goodhue Hall, 44.462356, -93.149601);
+INSERT INTO buildings (id, building, geo_lat, geo_long) VALUES (16, Hill House, 44.458091, -93.155099);
+INSERT INTO buildings (id, building, geo_lat, geo_long) VALUES (17, Hunt House, 44.458688, -93.158254);
+INSERT INTO buildings (id, building, geo_lat, geo_long) VALUES (18, Huntington House, 44.458692, -93.155137);
+INSERT INTO buildings (id, building, geo_lat, geo_long) VALUES (19, Memorial Hall, 44.460028, -93.151822);
+INSERT INTO buildings (id, building, geo_lat, geo_long) VALUES (20, Musser Hall, 44.459913, -93.157234);
+INSERT INTO buildings (id, building, geo_lat, geo_long) VALUES (21, Myers Hall, 44.460679, -93.150750);
+INSERT INTO buildings (id, building, geo_lat, geo_long) VALUES (22, Nason House, 44.459799, -93.157502);
+INSERT INTO buildings (id, building, geo_lat, geo_long) VALUES (23, Nourse Hall, 44.460471, -93.151879);
+INSERT INTO buildings (id, building, geo_lat, geo_long) VALUES (24, Owens House, 44.459490, -93.158008);
+INSERT INTO buildings (id, building, geo_lat, geo_long) VALUES (25, Parish House, 44.457667, -93.154666);
+INSERT INTO buildings (id, building, geo_lat, geo_long) VALUES (26, Rice House, 44.458003, -93.155858);
+INSERT INTO buildings (id, building, geo_lat, geo_long) VALUES (27, Scott House, 44.459871, -93.157713);
+INSERT INTO buildings (id, building, geo_lat, geo_long) VALUES (28, Severance Hall, 44.460951, -93.156665);
+INSERT INTO buildings (id, building, geo_lat, geo_long) VALUES (29, Watson Hall, 44.459343, -93.150368);
+INSERT INTO buildings (id, building, geo_lat, geo_long) VALUES (30, Wilson House, 44.460306, -93.158092);
+INSERT INTO buildings (id, building, geo_lat, geo_long) VALUES (31, Sayles, 44.461225, -93.156087);
+INSERT INTO buildings (id, building, geo_lat, geo_long) VALUES (32, Rec, 44.464112, -93.149557);
+INSERT INTO buildings (id, building, geo_lat, geo_long) VALUES (33, Libe, 44.462346, -93.154654);
+INSERT INTO buildings (id, building, geo_lat, geo_long) VALUES (34, Weitz, 44.457108, -93.156158);
+INSERT INTO buildings (id, building, geo_lat, geo_long) VALUES (35, LDC, 44.461024, -93.151393);
+INSERT INTO buildings (id, building, geo_lat, geo_long) VALUES (36, Mudd, 44.460892, -93.152372);
+INSERT INTO buildings (id, building, geo_lat, geo_long) VALUES (37, Olin, 44.461303, -93.152846);
+INSERT INTO buildings (id, building, geo_lat, geo_long) VALUES (38, Hulings, 44.460834, -93.153118);
+INSERT INTO buildings (id, building, geo_lat, geo_long) VALUES (39, Music, 44.461374, -93.153445);
+INSERT INTO buildings (id, building, geo_lat, geo_long) VALUES (40, CMC, 44.462476, -93.153614);
+INSERT INTO buildings (id, building, geo_lat, geo_long) VALUES (41, Laird, 44.462097, -93.153919);
+INSERT INTO buildings (id, building, geo_lat, geo_long) VALUES (42, Leighton, 44.462073, -93.155571);
+INSERT INTO buildings (id, building, geo_lat, geo_long) VALUES (43, Bald Spot, 44.460982, -93.154605);
+INSERT INTO buildings (id, building, geo_lat, geo_long) VALUES (44, Cowling, 44.459929, -93.149681);
+INSERT INTO buildings (id, building, geo_lat, geo_long) VALUES (45, Concert Hall, 44.460227, -93.153227);
+INSERT INTO buildings (id, building, geo_lat, geo_long) VALUES (46, Chapel, 44.460231, -93.154729);
+INSERT INTO buildings (id, building, geo_lat, geo_long) VALUES (47, Mini Bald Spot, 44.460469, -93.151296);
+INSERT INTO buildings (id, building, geo_lat, geo_long) VALUES (48, Arb, 44.463551, -93.150009);
+INSERT INTO buildings (id, building, geo_lat, geo_long) VALUES (49, Laird Football Stadium, 44.461717, -93.158474);
+INSERT INTO buildings (id, building, geo_lat, geo_long) VALUES (50, Baseball/Softball Fields, 44.466131, -93.146956);
+INSERT INTO buildings (id, building, geo_lat, geo_long) VALUES (51, Bell Field, 44.460103, -93.148772);
+INSERT INTO buildings (id, building, geo_lat, geo_long) VALUES (52, Hill of Three Oaks, 44.463306, -93.147189);
+INSERT INTO buildings (id, building, geo_lat, geo_long) VALUES (53, Little Bald Spot, 44.460381, -93.151221);
+INSERT INTO buildings (id, building, geo_lat, geo_long) VALUES (54, Lyman Lakes, 44.462836, -93.151144);
---
+++ roomdraw_process/make_sql_files.py
@@ -0,0 +1,90 @@
+# SQL file creator
+# CS257, Fall 2013, Jeff Ondich
+# Erin Wilson, Jiatao Cheng, Adam Canady
+
+import csv
+
+document_names = ['rooms.csv', 'buildings.csv', 'number_map.csv']
+
+#### Create rooms.sql ####
+rooms_sql = open('rooms.sql','w')
+rooms_sql.write("DROP TABLE IF EXISTS rooms;\n")
+rooms_sql.write("""CREATE TABLE rooms (
+  building_id int,
+  avg_draw_number int,
+  room int,
+  occupancy int,
+  sub_free boolean,
+  quiet boolean
+);\n\n""")
+
+headers = True
+for line in csv.reader(open('rooms.csv','rU')):
+    if headers == True: headers = False; continue # Skip headers
+
+    line = [int(i) for i in line]
+
+    building_id = line[0]
+    avg_draw_number = line[1]
+    room = line[2]
+    occupancy = line[3]
+    sub_free = line[4]
+    quiet = line[5]
+
+    insert_statement = "INSERT INTO rooms (building_id, avg_draw_number, room, occupancy, sub_free, quiet) " + \
+                        "VALUES (%d, %d, %d, %d, %d, %d);\n" \
+                        % (building_id, avg_draw_number, room, occupancy, sub_free, quiet)
+
+    rooms_sql.write(insert_statement)
+
+rooms_sql.close()
+
+#### Create buildings.sql ####
+buildings_sql = open('buildings.sql','w')
+buildings_sql.write("DROP TABLE IF EXISTS buildings;\n")
+buildings_sql.write("""CREATE TABLE buildings (
+  id int,
+  building text,
+  geo_lat float,
+  geo_long float
+);\n\n""")
+
+headers = True
+for line in csv.reader(open('buildings.csv','rU')):
+    if headers == True: headers = False; continue # Skip headers
+
+    id = int(line[0])
+    building = line[1]
+    geo_lat = float(line[2])
+    geo_long = float(line[3])
+
+    insert_statement = "INSERT INTO buildings (id, building, geo_lat, geo_long) " + \
+                        "VALUES (%d, %s, %f, %f);\n" \
+                        % (id, building, geo_lat, geo_long)
+
+    buildings_sql.write(insert_statement)
+
+buildings_sql.close()
+
+#### Create number_map.sql ####
+number_map_sql = open('number_map.sql','w')
+number_map_sql.write("DROP TABLE IF EXISTS number_map;\n")
+number_map_sql.write("""CREATE TABLE number_map (
+  db_num int,
+  roomdraw_num int
+);\n\n""")
+
+headers = True
+for line in csv.reader(open('number_map.csv','rU')):
+    if headers == True: headers = False; continue # Skip headers
+
+    db_num = int(line[0])
+    roomdraw_num = int(line[1])
+
+    insert_statement = "INSERT INTO number_map (db_num, roomdraw_num) "+ \
+                        "VALUES (%d, %d);\n" \
+                        % (db_num, roomdraw_num)
+
+    number_map_sql.write(insert_statement)
+
+number_map_sql.close()
